03004baefe22d1917cc76b56330cafc9ac4671ee
Filip <filip.grgic3@gmail.com>
2024-04-27 21:29:43 +0200

    Finished first bonus exercise (DIMACS Generator)

---
+++ dimacs_generator.py
@@ -0,0 +1,52 @@
+def queens(n):
+    rows = horizontal(n) + vertical(n) + diagonal(n)
+    with open(f"queens_{n}.cnf", "w") as f:
+        f.write(f"p cnf {n*n} {len(rows)}\n")
+        for row in rows:
+            f.write(" ".join(map(str, row)) + "\n")# + " 0\n")
+
+
+def horizontal(n):
+    result = []
+    for i in range(n):
+        next_row = [i*n + j for j in range(1, n+1)]
+        result.append(next_row)
+
+        for a in range(len(next_row)):
+            for b in range(a+1, len(next_row)):
+                result.append([-next_row[a], -next_row[b]])
+
+    return result
+
+def vertical(n):
+    result = []
+    for i in range(1, n+1):
+        next_row = [i + j * n for j in range(0, n)]
+        result.append(next_row)
+
+        for a in range(len(next_row)):
+            for b in range(a + 1, len(next_row)):
+                result.append([-next_row[a], -next_row[b]])
+
+    return result
+
+def diagonal(n):
+    result = []
+    for i in range(1, pow(n, 2) + 1):
+        if i % n == 0:
+            continue
+        for j in range(i + n + 1, pow(n, 2) + 1, n+1):
+            result.append([-i, -j])
+
+    for i in range(2, pow(n, 2)):
+        if i % n == 1:
+            continue
+        for j in range(i + n - 1, pow(n, 2) + 1, n-1):
+            result.append([-i, -j])
+            if j % n == 1:
+                break
+
+    return result
+
+if __name__ == "__main__":
+    queens(3)
